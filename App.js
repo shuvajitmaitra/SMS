5365d481d222706be33a4954c327cd7ca9669642
Shuvajit Maitra <shuvajitmaitra@gmail.com>
2024-07-01 21:22:44 +0600

    font family added

--- App.js
+++ App.js
@@ -1,15 +1,39 @@
+import React, { useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor, store } from "./redux/store";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded, error] = useFonts({
+    poppins_regular: require("./assets/Fonts/Poppins-Regular.ttf"),
+    poppins_medium: require("./assets/Fonts/Poppins-Medium.ttf"),
+    poppins_semiBold: require("./assets/Fonts/Poppins-SemiBold.ttf"),
+    poppins_bold: require("./assets/Fonts/Poppins-Bold.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded && !error) {
+    return null;
+  }
+
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <View style={styles.container}>
-          <Text>Open up App.js to start working on your app!</Text>
+          <Text style={styles.customText}>
+            Open up App.js to start working on your app!
+          </Text>
           <StatusBar style="auto" />
         </View>
       </PersistGate>
@@ -23,5 +47,9 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  customText: {
+    fontFamily: "poppins_regular",
+    fontSize: 20,
   },
 });
