6326b4ef637f30bebdf0cfd86e60109a45d6ae61
Shuvajit Maitra <shuvajitmaitra@gmail.com>
2024-07-01 22:44:57 +0600

    navigation and routes set

--- Navigations/Routes.js
+++ Navigations/Routes.js
@@ -3,31 +3,14 @@ import { View, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Fonts from "../constants/Fonts";
-
-function HomeScreen() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        // backgroundColor: "red",
-      }}
-    >
-      <Text style={{ fontFamily: Fonts.REGULAR, fontSize: 30 }}>
-        Home Screen
-      </Text>
-    </View>
-  );
-}
+import AuthStack from "./AuthStack";
 
 const Stack = createNativeStackNavigator();
-
 function Routes() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <>{AuthStack(Stack)}</>
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ Screens/index.js
@@ -0,0 +1,5 @@
+// Auth Screen
+export { default as InitialScreen } from "./AuthScreen/InitialScreen";
+
+// Chat Screen
+export { default as ChatScreen } from "./ChatScreen/ChatScreen";
